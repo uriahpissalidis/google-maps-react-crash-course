44ba7ddb62588d1c69aa470fe2503ebc95e6e014
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-07-20 19:27:46 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "../components/map";
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
